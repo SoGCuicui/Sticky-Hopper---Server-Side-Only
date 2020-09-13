9085443f2f6c34f9845a36f75434594f755a7a68
Cuicui <cuicui73@gmail.com>
2020-09-13 08:07:06 -0400

    Cleanup

--- src/main/java/ovh/cuicui/stickyhopper/Main.java
+++ src/main/java/ovh/cuicui/stickyhopper/Main.java
@@ -8,19 +8,17 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.registry.Registry;
 
 public class Main implements ModInitializer {
-	private static String ID = "stickyhopper:sticky_hopper";
-	private static String ENTITY_ID = "stickyhopper:sticky_hopper_entity";
+	private static String STICKY_HOPPER_ID = "stickyhopper:sticky_hopper";
+	private static String STICKY_HOPPER_ENTITY_ID = "stickyhopper:sticky_hopper_entity";
 
 	private static final StickyHopperBlock STICKY_HOPPER_BLOCK = new StickyHopperBlock();
-	public static final BlockEntityType<StickyHopperBlockEntity> STICKY_HOPPER_BLOCK_ENTITY = BlockEntityType.Builder
-			.create(StickyHopperBlockEntity::new, STICKY_HOPPER_BLOCK).build(null);
+	public static final BlockEntityType<StickyHopperBlockEntity> STICKY_HOPPER_BLOCK_ENTITY = BlockEntityType.Builder.create(StickyHopperBlockEntity::new, STICKY_HOPPER_BLOCK).build(null);
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.BLOCK, ID, STICKY_HOPPER_BLOCK);
-		Registry.register(Registry.ITEM, ID, new BlockItem(STICKY_HOPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
-
-		Registry.register(Registry.BLOCK_ENTITY_TYPE, ENTITY_ID, STICKY_HOPPER_BLOCK_ENTITY);
+		Registry.register(Registry.BLOCK, STICKY_HOPPER_ID, STICKY_HOPPER_BLOCK);
+		Registry.register(Registry.ITEM, STICKY_HOPPER_ID, new BlockItem(STICKY_HOPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
+		Registry.register(Registry.BLOCK_ENTITY_TYPE, STICKY_HOPPER_ENTITY_ID, STICKY_HOPPER_BLOCK_ENTITY);
 
 		System.out.println("Sticky Hopper mod loaded!");
 	}
